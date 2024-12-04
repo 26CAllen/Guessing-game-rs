a5dab18ac8a535eb553bc4d11ac213acb1592e58
26JEndic <jeremyendicott07@gmail.com>
2024-12-04 18:04:23 +0000

    Added gameLogic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn gameLogic(str_input: &str) {
+    let input_res = str_input.parse::<i32>();
+    let input = match input_res {
+        Ok(v) => v,
+        Err(_) => {, 1}
+    }
+}
