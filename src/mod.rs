8e0aed932184e26fcacd07dd23ce7fc2ff6a13e3
Caleb Allen <caleb.allen@stu.rowan.kyschools.us>
2024-12-09 17:44:28 +0000

    edit mod.rs

--- src/mod.rs
+++ src/mod.rs
@@ -1,1 +1,15 @@
-pub mod takeinput()
+pub mod TakeInput {
+    fn takeInput() -> Result<i64, ()> {
+        use std::io;
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        let handle = stdin.lock();
+        handle.read_line(&mut buffer)?;
+        let input: &str = &buffer[..];
+        let r_number = str::parse::<i64>();
+        let number = match r_number {
+            Ok(v) => v,
+            Err(_) => {println!("Make sure you input a number"); self::takeInput()}
+        };
+    }
+}
