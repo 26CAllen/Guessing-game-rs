717e3d1d881c236526c0d47f70eba6dd1096175e
Caleb Allen <caleb.allen@stu.rowan.kyschools.us>
2024-12-09 18:02:24 +0000

    changed for conventions and fixed game logic

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+mod take_input;
+
 fn main() {
+    use take_input::take_input::take_input;
     use rand;
-    let answer = (rand::random() % 10000) + 1;
-    gameLogic(input, answer);
+    let input = take_input();
+    let answer: i64 = (rand::random::<i64>() % 10000) + 1;
+    while game_logic(input, answer) != true {
+        continue;
+    }
 }
 
-fn gameLogic(input: i64, answer: i64) {
+fn game_logic(input: i64, answer: i64) -> bool {
     if input==answer {
         // win condition
-        println!("Correct!")
+        println!("Correct!");
+        return true;
     }
     match input > answer {
-       True => println!("Too high!"),
-       False => println!("Too low!")
+       true => println!("Too high!"),
+       false => println!("Too low!")
     }
+    false
 }
---
+++ src/take_input.rs
@@ -0,0 +1,21 @@
+pub mod take_input {
+    pub fn take_input() -> i64 {
+        use std::io::BufRead;
+        use std::io;
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        let result = handle.read_line(&mut buffer);
+        match result {
+            Ok(_) => {}
+            Err(_) => {println!("Make sure you input a number"); self::take_input();}
+        };
+        let input: &str = &buffer[..];
+        let r_number = str::parse::<i64>(input);
+        let number = match r_number {
+            Ok(v) => v,
+            Err(_) => {println!("Make sure you input a number"); self::take_input(); 0}
+        };
+        return number
+    }
+}
