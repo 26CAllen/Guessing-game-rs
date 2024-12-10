244f9597f3e35e02339f23104349fe37aebc3764
26JEndic <jeremyendicott07@gmail.com>
2024-12-10 17:31:09 +0000

    poopy caca

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 fn main() {
+    let counter: i64 = 0;
+
     use rand;
     let answer = (rand::random() % 10000) + 1;
-    gameLogic(input, answer);
+    gameLogic(input, answer, counter);
 }
 
-fn gameLogic(input: i64, answer: i64) {
+fn gameLogic(input: i64, answer: i64, mut counter: i64) {
     if input==answer {
         // win condition
-        println!("Correct!")
+        println!("Correct!");
+        println("It took you")
+        return;
     }
+
+    counter += 1;
+
     match input > answer {
        True => println!("Too high!"),
        False => println!("Too low!")
